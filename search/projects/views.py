8ff34c1b32e2801b5cfbba16064ed8ec687f186f
PUSH-YA <91928008+PUSH-YA@users.noreply.github.com>
2024-01-27 23:07:41 -0800

    updated html pages to render data from the database

--- search/projects/views.py
+++ search/projects/views.py
@@ -1,19 +1,12 @@
 from django.shortcuts import render
-
-projects_list = [{'id':'1', 'title':'finance model', 'description':'Model your personal finance'}
-,{'id':'2', 'title':'Portfolio Website', 'description':'Just keep memorising frameworks'}
-,{'id':'3', 'title':'ASL translator', 'description':'train a CNN to recognise the ASL'}
-]
+from .models import *
 
 def projects(request):
-    page = 'Projects'
-    number = 10
-    context = {'page':page, 'number':number, 'projects':projects_list}
+    projects = Project.objects.all()
+    context = {'projects':projects}
     return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
-    projectObj = None
-    for i in projects_list:
-        if i['id'] == pk:
-            projectObj = i
-    return render(request, 'projects/single-project.html', {'project': projectObj})
+    projectObj = Project.objects.get(id = pk)
+    tags = projectObj.tags.all()
+    return render(request, 'projects/single-project.html', {'project': projectObj, 'tags':tags})
