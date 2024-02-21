b944b9cb15fbfad4c88dce56038aea11a92dedf4
PUSH-YA <pushya22.jain@gmail.com>
2024-02-21 14:59:32 -0800

    Update project form styling and functionality

--- search/projects/forms.py
+++ search/projects/forms.py
@@ -1,8 +1,20 @@
 from django.forms import ModelForm
 from .models import Project
+from django import forms
 
 class ProjectForm(ModelForm):
     # Helps understand which data model to store / validate / fill
     class Meta:
         model = Project
         fields = ['title', 'feature_image', 'description','demo_link','source_link','tags']
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple()
+        }
+    
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+        # self.fields['title'].widget.attrs.update({'class':'input', 'placeholder':'Add title'})
+        # self.fields['description'].widget.attrs.update({'class':'input', 'placeholder':'Add description'})
+
+        for name,field in self.fields.items():
+            field.widget.attrs.update({'class':'input'})
